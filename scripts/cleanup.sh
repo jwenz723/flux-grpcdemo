047072d962c6234406975d39e88b6097bc861614
jwenz723 <jwenz723@gmail.com>
2019-07-08 12:30:03 -0600

    reordered cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,11 +2,12 @@
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-helm delete --purge promop
 helm delete --purge grpcdemo-client
 helm delete --purge grpcdemo-server
-helm delete --purge flux
-
 kubectl delete ns grpcdemo
+
+helm delete --purge promop
 kubectl delete ns promop
+
+helm delete --purge flux
 kubectl delete ns flux
